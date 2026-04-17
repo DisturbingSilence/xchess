b654b6f05b7296d842266dc6db1d102b179e88e3
kebabs228 <davidenkokirill243@gmail.com>
2026-04-17 23:46:27 +0300

    Пешка ходит офигеть

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
+#include <stdlib.h>
 #include <stdio.h>
 #define row 8
 #define column 8
+#define RED   "\033[31m"
+#define WHITE "\033[37m"
+#define RESET "\033[0m"
+#define BROWN "\033[38;5;94m"
 
 enum
 {
@@ -80,21 +85,25 @@ char getPieceChar(ChessPiece p)
 void out()
 {
     printf("\n   ");
-    for(char i='a'; i<='h'; i++) printf("%c ", i);
+    for(char i='a'; i<='h'; i++) printf(BROWN"%c "RESET, i);
     printf("\n");
 
     for(int i=0; i<row; i++)
     {
-        printf("%i  ", 8-i);
+        printf(BROWN"%i  "RESET, 8-i);
         for(int j=0; j<column; j++)
         {
-            printf("%c ", getPieceChar(A[i*row+j]));
+            if((A[i*row+j]& 1)==0)
+                printf("%c ", getPieceChar(A[i*row+j]));
+            else
+                printf(RED"%c "RESET, getPieceChar(A[i*row+j]));
+
         }
-        printf(" %i\n", 8-i);
+        printf(BROWN" %i\n"RESET, 8-i);
     }
 
     printf("   ");
-    for(char i='a'; i<='h'; i++) printf("%c ", i);
+    for(char i='a'; i<='h'; i++) printf(BROWN"%c "RESET, i);
     printf("\n\n");
 }
 // сделать базовый принцип ходьбы фигур
@@ -106,26 +115,97 @@ int LogicPAWN(int x,int y)
         return 0;
     if((A[x] & 1) == 0)
     {
-        if((x-16==y || x-8==y) && getPieceChar(A[y])=='.')
+        if(((x-16==y || x-8==y) && getPieceChar(A[y])=='.') && ((x/7!=7 && (x-y)/8!=2) || (x/7==7)))
             return 1;
-            if((x-7==y && getPieceChar(A[y])!='.') || (x-9==y && getPieceChar(A[y])!='.'))
-                return 1;
-                return 0;
-            }
+        if((x-7==y && getPieceChar(A[y])!='.') || (x-9==y && getPieceChar(A[y])!='.'))
+            return 1;
+        return 0;
+    }
     else
     {
-        if((x+16==y || x+8==y) && getPieceChar(A[y])=='.')
+        if(((x+16==y || x+8==y) && getPieceChar(A[y])=='.') &&((x/7!=1 && (y-x)/8!=2) || (x/7==1)))
             return 1;
-            if((x+9==y && getPieceChar(A[y])!='.') || (x+7==y && getPieceChar(A[y])!='.'))
-                return 1;
-                return 0;
-            }
+        if((x+9==y && getPieceChar(A[y])!='.') || (x+7==y && getPieceChar(A[y])!='.'))
+            return 1;
+        return 0;
+    }
     return 0;
+}
+
+int replace(int x, int y)
+{
+    A[y]=A[x];
+    A[x]=EMPTY;
+}
+
+int Figaro(int x, int y)
+{
+    char c=getPieceChar(A[x]);
+    switch (c)
+    {
+    case 'P':
+    {
+        if (LogicPAWN(x,y)==1)
+        {
+            replace(x,y);
+            return 1;
+        }
+    }
+    default:
+        return 0;
+    }
+}
+
+int cordfinder(int n, char temp)
+{
+    return (8-n)*row+(temp-'a');
+}
+// Надо получить ходы, проверь правильность, записать, поменять сторону хода
+void move()
+{
+    while(1)
+    {
+        system("cls");
+        out();
+        int startcord, endcord;
+        printf("\n Enter your move(for example e2e4):");
+        char spos[3], epos[3];
+        scanf("%2s%2s", spos, epos);
+        //printf("%s", spos);
+        char temp=spos[0];
+        int n=spos[1]-'0';
+        int f1=0, f2=0;
+        for(int i=1; i<=8; i++)
+            if(n==i)
+                f1=1;
+        for(char i='a'; i<='h'; i++)
+            if(temp==i)
+                f2=1;
+        if(f1==1 && f2==1)
+            startcord=cordfinder(n, temp);
+        else
+            continue;
+        temp=epos[0];
+        n=epos[1]-'0';
+        f1=0;
+        f2=0;
+        for(int i=1; i<=8l; i++)
+            if(n==i)
+                f1=1;
+        for(char i='a'; i<='h'; i++)
+            if(temp==i)
+                f2=1;
+        if(f1==1 && f2==1)
+            endcord=cordfinder(n, temp);
+        else
+            continue;
+        printf("start cord=%i, end cord=%i", startcord, endcord);
+        Figaro(startcord,endcord);
+    }
 }
 
 int main()
 {
-    out();
-    printf("\n\n%d",LogicPAWN(0,7));
+    move();
     return 0;
 }
